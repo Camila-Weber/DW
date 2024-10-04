e0e92d7e90249138859d97bbd78ea8b3564f785d
Camila Weber <camilawe2004@gmail.com>
2024-10-04 08:49:22 -0400

    detalhes

--- Exercicio_JS/main.js
+++ Exercicio_JS/main.js
@@ -6,11 +6,21 @@ function mudarTexto(){
 var num = 0;
 function contar(){
     num++;
-    document.getElementById("incremento").innerHTML = "Número de cliques " + num;
+    document.getElementById("like").innerHTML = num + " gostei";
 }
 
+var num2 = 0;
+function contar2(){
+    num2++;
+    document.getElementById("deslike").innerHTML = num2 + " não gostei";
+}
+
+var resultado = document.getElementById("resultado");
+resultado.style.display = "none";
 function escrever(){
     var nome = document.getElementById("nome");
     var email = document.getElementById("email");
-    document.getElementById("escrever").innerHTML = "Nome: " + nome.value + "<br>Email: " + email.value;
+    var comentario = document.getElementById("comentario");
+    resultado.innerHTML = "Comentário de " + nome.value + ";<br>" + email.value + ";<br><br><br>'" + comentario.value + "'";
+    resultado.style.display = "block";
 }
